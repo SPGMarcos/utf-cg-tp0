69dca9a138a69a284db10d56da35682ba718d4bd
Marcos Gabriel <mgfm554@gmail.com>
2026-03-16 00:48:14 -0300

    Adicionar as abelhas

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+let mouseX = 0;
+let mouseY = 0;
+
+document.addEventListener("mousemove", function(e) {
+
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+
+});
+
+function follow(beeId, speed){
+
+    let bee = document.getElementById(beeId);
+
+    let posX = 0;
+    let posY = 0;
+
+    setInterval(function(){
+
+        posX += (mouseX - posX) * speed;
+        posY += (mouseY - posY) * speed;
+
+        bee.style.left = posX + "px";
+        bee.style.top = posY + "px";
+
+        if(mouseX > posX){
+            bee.style.transform = "scaleX(-1)";
+        }else{
+            bee.style.transform = "scaleX(1)";
+        }
+
+    },20);
+
+}
+
+follow("bee",0.08);
+follow("bee2",0.05);
+follow("bee3",0.03);
+
+
 function acaoMisteriosa() {
 
     alert("Olá professor Flavio!!! 👋 Esse é o Getulio, a minha mini Torre de Hidroponia, suas mudas de morango e seu painel de controle ainda em desenvolvimento (click em ''OK'' para ver). O painel tambem foi desenvolvido com HTML, CSS e JavaScript, além da linguagem C++ para o controle do hardware.");
@@ -10,7 +50,6 @@ function acaoMisteriosa() {
         foto.src = "foto.jpg";
     }
 
-    let audio = new Audio("som.mp3");
-    audio.play();
+    let audio = new Audio("som.mp3").play();
 
 }
